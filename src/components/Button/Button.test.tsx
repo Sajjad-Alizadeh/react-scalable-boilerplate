e59456718a982dd7024895644dc3bb1a0b35c479
Sajjad Alizadeh <sajjadalizzh@gmail.com>
2023-08-28 12:49:52 +0330

    fix build errors

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
-import Details from './index'
+import Button from './index'
 
 test('renders learn react link', () => {
-  render(<Details />)
+  render(<Button />)
   const linkElement = screen.getByText(/learn react/i)
   expect(linkElement).toBeInTheDocument()
 })
---
+++ src/pages/Details/types.ts
@@ -0,0 +1,3 @@
+export default interface Details {
+  title: string
+}
